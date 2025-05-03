894909d7f9f93cbf07ee1b4b1a66e3a2ca7a57f2
0-Meng-bits <mugiwaraomeng@gmail.com>
2025-05-03 16:25:19 +0800

    Create rent.aspx.cs

---
+++ rent.aspx.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Web.UI;
+
+namespace WebApplication1.VESTASIM
+{
+    public partial class Rent : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["ClientID"] != null)
+                {
+                    loginPanel.Visible = false;
+                    logoutPanel.Visible = true;
+                }
+                else
+                {
+                    loginPanel.Visible = true;
+                    logoutPanel.Visible = false;
+                }
+            }
+        }
+    }
+}
